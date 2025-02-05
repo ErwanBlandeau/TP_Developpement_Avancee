de49e612e8e3367e304e8faf1da810cbaea3900c
Blandeau Erwan <130489326+ErwanBlandeau@users.noreply.github.com>
2025-02-05 14:40:13 +0100

    test: add unit tests for getRankingData and updateRank methods in CacheRankingService

--- apps/realtime-elo-ranker-server/src/services/ranking-cache/ranking-cache.service.spec.ts
+++ apps/realtime-elo-ranker-server/src/services/ranking-cache/ranking-cache.service.spec.ts
@@ -26,15 +26,6 @@ describe('CacheRankingService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
-  describe('setRankingData', () => {
-    it('should set ranking data', async () => {
-      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue({} as Player);
-      await service.setRankingData('player1', 1000);
-      expect(saveSpy).toHaveBeenCalledWith(expect.objectContaining({ name: 'player1', rank: 1000 }));
-    });
-  });
-
   describe('getRankingData', () => {
     it('should get ranking data', async () => {
       const findSpy = jest.spyOn(repository, 'find').mockResolvedValue([{ name: 'player1', rank: 1000 }] as Player[]);
@@ -44,6 +35,25 @@ describe('CacheRankingService', () => {
     });
   });
 
+  describe('setRankingData', () => {
+    it('should set ranking data', async () => {
+      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue({} as Player);
+      await service.setRankingData('player1', 1000);
+      expect(saveSpy).toHaveBeenCalledWith(expect.objectContaining({ name: 'player1', rank: 1000 }));
+    });
+  });
+  describe('updateRank', () => {
+    it('should update rank', async () => {
+      const player = { name: 'player1', rank: 1000 } as Player;
+      const findOneSpy = jest.spyOn(repository, 'findOne').mockResolvedValue(player);
+      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue(player);
+      await service.updateRank('player1', 1500);
+      expect(findOneSpy).toHaveBeenCalledWith({ where: { name: 'player1' } });
+      expect(saveSpy).toHaveBeenCalledWith(expect.objectContaining({ name: 'player1', rank: 1500 }));
+    });
+  });
+
+  
   describe('getRankingMoyen', () => {
     it('should get average ranking', async () => {
       const findSpy = jest.spyOn(repository, 'find').mockResolvedValue([{ name: 'player1', rank: 1000 }, { name: 'player2', rank: 2000 }] as Player[]);
@@ -60,14 +70,5 @@ describe('CacheRankingService', () => {
     });
   });
 
-  describe('updateRank', () => {
-    it('should update rank', async () => {
-      const player = { name: 'player1', rank: 1000 } as Player;
-      const findOneSpy = jest.spyOn(repository, 'findOne').mockResolvedValue(player);
-      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue(player);
-      await service.updateRank('player1', 1500);
-      expect(findOneSpy).toHaveBeenCalledWith({ where: { name: 'player1' } });
-      expect(saveSpy).toHaveBeenCalledWith(expect.objectContaining({ name: 'player1', rank: 1500 }));
-    });
-  });
+  
 });
